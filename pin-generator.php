a821a9e458cda49856fa61e25765e092f62e59e5
Ewere Diagboya <boya360@yahoo.com>
2014-01-10 13:06:34 +0000

    Create pin-generator.php

---
+++ pin-generator.php
@@ -0,0 +1,46 @@
+<?php
+mysql_connect("localhost","root","");
+
+// GUID Generator
+function generateGuid($include_braces = false) {
+    if (function_exists('com_create_guid')) {
+        if ($include_braces === true) {
+            return com_create_guid();
+        } else {
+            return substr(com_create_guid(), 1, 36);
+        }
+    } else {
+        mt_srand((double) microtime() * 10000);
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+       
+        $guid = substr($charid,  0, 8) . '' .
+                substr($charid,  8, 4) . '' .
+                substr($charid, 12, 4) . '' .
+                substr($charid, 16, 4) . '' .
+                substr($charid, 20, 12);
+ 
+        if ($include_braces) {
+            $guid = '{' . $guid . '}';
+        }
+   
+        return $guid;
+    }
+}
+
+if(isset($_POST['genrate'])) {
+	$pindigit = $_POST['pindigit'];
+	$pintotal = $_POST['pintotal'];
+	for ($i=0;$i<=$pintotal; $i++) {
+	$pin =  str_replace('-', '', generateGuid())  . "<br />";
+	$spin = substr($pin,0,$pindigit) . '<br />';
+	echo $spin;
+}
+}
+?>
+<html>
+<form method="post">
+Enter number of PINs to Generate: <input name="pintotal" type="text" /><br />
+PIN Digits: <input name="pindigit" type="text" /><br />
+<input  type="submit" name="genrate" value="Generate" />
+</form>
+</html>
